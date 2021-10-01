ecfde84bfacb0d9b75adfad9233658c78a9ed81c
yjyjyj47 <84319605+yjyjyj47@users.noreply.github.com>
2021-10-02 01:58:57 +0900

    app end right after running it

--- app/src/main/java/com/example/androidcopy/MainActivity.kt
+++ app/src/main/java/com/example/androidcopy/MainActivity.kt
@@ -3,24 +3,38 @@ package com.example.androidcopy
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidcopy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding= ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_action_bar,menu)
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        //menuItems 뷰객체 포획
+        val menuItem = menu.findItem(R.id.menu_search)
+        //액션뷰를 서치뷰로 캐스팅 (action 시 서치뷰 display)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
 
-        return super.onCreateOptionsMenu(menu)
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+        })
+
+        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        Log.d("main","onSupportNavigateUp Called")
+        Log.d("main", "onSupportNavigateUp Called")
         return super.onSupportNavigateUp()
     }
 }
